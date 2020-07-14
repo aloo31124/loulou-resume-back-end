678c0af452fe54b481ecf573b2d0e1d083b9ad4c
loulou <loulou@sun.net.tw>
2020-07-14 11:02:55 +0800

    [撰寫自傳] [功能] [ 新增章節] 1.按下 [ 新增章節] 按鈕，顯示 ck editor 2.防止 ck editor 連續按 3.取消復原

--- resources/views/autobiographyIndex.blade.php
+++ resources/views/autobiographyIndex.blade.php
@@ -2,15 +2,42 @@
 
 @section('content')
 <script src="{{ asset('ckeditor/ckeditor.js') }}"></script>
+<script>
 
+function openAddChapterTextArea(){
+    if($('#IsAddChapterTextAreaOpen').val() == 'false'){
+        $("#addChapterTextArea").append(
+            "<textarea class='form-control' id='CKEditor'></textarea>"
+        );
+        $("#addChapterTextArea").after(
+            "<button type='button' class='btn btn-danger' id='cancelAddChapter' onclick='closeAddChapterTextArea()' >取消</button>" +
+            "<button type='button' class='btn btn-info' id='submitAddChapter' onclick='' >儲存</button>"
+        );
+        CKEDITOR.replace( 'CKEditor' );
+        $('#IsAddChapterTextAreaOpen').val('true');
+    }
+}
 
-test
-<div class="form-group">
-    <textarea class="form-control" id="editor1" name="editor1"></textarea>
+function closeAddChapterTextArea(){
+    if($('#IsAddChapterTextAreaOpen').val() == 'true'){
+        $('#IsAddChapterTextAreaOpen').val('false');
+        $('#addChapterTextArea').replaceWith(
+            "<div id='addChapterTextArea' class='form-group' style='margin-top:20px'></div>"
+        );
+        $('#cancelAddChapter').remove();
+        $('#submitAddChapter').remove();
+    }
+}
+
+</script>
+
+<h2 style="margin-top:20px">撰寫自傳</h2>
+<div class="row" style="margin-top:20px">
+    <button type='button' class='btn btn-info' id='addChapter' onclick='openAddChapterTextArea()' >新增章節</button> 
 </div>
 
-<script>
-    CKEDITOR.replace( 'editor1' );
-</script>
+<input type='hidden' id='IsAddChapterTextAreaOpen' value='false' >
+<div id='addChapterTextArea' class='form-group' style='margin-top:20px'></div>
+
 
 @endsection
