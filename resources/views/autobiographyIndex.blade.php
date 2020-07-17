59924adddf50eb82fbaa3f20bd3c6f40729418db
loulou <loulou@sun.net.tw>
2020-07-17 21:23:50 +0800

    [撰寫自傳 > 新增章節功能 改 modal 視窗] 1.更新用 location.reload(); 2.data-backdrop="static"  可讓 modal 視窗不被點選關閉 3.data-dismiss="modal" 用於 在按鈕中關閉視窗。

--- resources/views/autobiographyIndex.blade.php
+++ resources/views/autobiographyIndex.blade.php
@@ -4,37 +4,6 @@
 <script src="{{ asset('ckeditor/ckeditor.js') }}"></script>
 <script>
 
-function openAddChapterTextArea(){
-    if($('#IsAddChapterTextAreaOpen').val() == 'false'){
-        $("#addChapterTextArea").append(
-            "<div class='input-group mb-3'>" +
-                "<div class='input-group-prepend'>" +
-                    "<span class='input-group-text' >章節名稱</span>" +
-                "</div>" +
-                "<input type='text' class='form-control' placeholder='請輸入新增章節名稱' id='CKEditorTitle' >" +
-            "</div>" +
-            "<textarea class='form-control' id='CKEditor'></textarea>"
-        );
-        $("#addChapterTextArea").after(
-            "<button type='button' class='btn btn-danger' id='cancelAddChapter' onclick='closeAddChapterTextArea()' >取消</button>" +
-            "<button type='button' class='btn btn-info' id='submitAddChapter' onclick='submitAddChapterTextArea()' >儲存</button>"
-        );
-        CKEDITOR.replace( 'CKEditor' );
-        $('#IsAddChapterTextAreaOpen').val('true');
-    }
-}
-
-function closeAddChapterTextArea(){
-    if($('#IsAddChapterTextAreaOpen').val() == 'true'){
-        $('#IsAddChapterTextAreaOpen').val('false');
-        $('#addChapterTextArea').replaceWith(
-            "<div id='addChapterTextArea' class='form-group' style='margin-top:20px'></div>"
-        );
-        $('#cancelAddChapter').remove();
-        $('#submitAddChapter').remove();
-    }
-}
-
 function submitAddChapterTextArea(){
     var CKEditorContent = CKEDITOR.instances.CKEditor.getData();
     var CKEditorTitle = $("#CKEditorTitle").val();
@@ -51,7 +20,7 @@ function submitAddChapterTextArea(){
             if(result!=1){
                 console.log("submitAddChapterTextArea not success");
             }
-            closeAddChapterTextArea();
+            location.reload();
         },error:function(){
             console.log("submitAddChapterTextArea error");
         }
@@ -63,7 +32,35 @@ function submitAddChapterTextArea(){
 
 <h2 style="margin-top:20px">撰寫自傳</h2>
 <div class="row" style="margin-top:20px">
-    <button type='button' class='btn btn-info' id='addChapter' onclick='openAddChapterTextArea()' >新增章節</button> 
+    <button type='button' class='btn btn-info' id='addChapter' onclick='' data-toggle="modal" data-target="#addChapterModal" >新增章節</button>     
+    
+    <!-- 新增章節 Modal 視窗 start -->
+    <div class="modal fade bd-example-modal-xl" id="addChapterModal" tabindex="-1" role="dialog" aria-hidden="true" data-backdrop="static"  >
+        <div class="modal-dialog modal-xl">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">新增章節</h5>
+                </div>            
+                <div class="modal-body">
+                    <div class='input-group mb-3'>
+                        <div class='input-group-prepend'>
+                            <span class='input-group-text' >章節名稱</span>
+                        </div>
+                        <input type='text' class='form-control' placeholder='請輸入新增章節名稱' id='CKEditorTitle' >
+                    </div>
+                    <textarea class='form-control' id='CKEditor' ></textarea>
+                    <script>
+                        CKEDITOR.replace( 'CKEditor' );
+                    </script>
+                </div>
+                <div class="modal-footer">
+                    <button type='button' class='btn btn-danger' id='cancelAddChapter' data-dismiss="modal" onclick='' >取消</button>
+                    <button type='button' class='btn btn-info' id='submitAddChapter' data-dismiss="modal" onclick='submitAddChapterTextArea()' >儲存</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- 新增章節 Modal 視窗 end -->
 </div>
 
 <input type='hidden' id='IsAddChapterTextAreaOpen' value='false' >
