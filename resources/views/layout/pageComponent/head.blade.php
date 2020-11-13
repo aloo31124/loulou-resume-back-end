db691ab8cd3228d8d9d523320c94757eab8d7015
loulou <loulou@sun.net.tw>
2020-11-13 14:39:48 +0800

    [架構] 將 bootsrap 與 jquery 加入專案

--- resources/views/layout/pageComponent/head.blade.php
+++ resources/views/layout/pageComponent/head.blade.php
@@ -2,7 +2,8 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>loulou resume</title>
 <link rel="stylesheet" href="css/style.css" >
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<link rel="stylesheet" href="bootstrap-4.2.1-dist\css\bootstrap.min.css" >
+<link rel="stylesheet" href="bootstrap-4.2.1-dist\js\bootstrap.min.js" >
+<script src="jquery\jquery-3.5.1.min.js"></script>
 
 
